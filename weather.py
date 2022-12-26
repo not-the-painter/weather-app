e0f94fdc7b9a218868e2b384d3725a9d7d6f0746
not_the_painter <bobross44@protonmail.com>
2022-12-25 23:17:05 -0500

    Put the api request into a functon

--- weather.py
+++ weather.py
@@ -1,29 +1,28 @@
 import json
 import os
-import webbrowser
+import tkinter
 
 import requests
 
-city = input("Enter the city: ")
-KEY = os.environ.get("WEATHER_API_KEY")
-BASE_URL = "https://api.weatherapi.com/v1/"
-CURRENT = "current.json"
-PARAMS = f"?key={KEY}&q={city}"
 
-URL = f"{BASE_URL}{CURRENT}{PARAMS}"
+def get_current_weather(city):
+    KEY = os.environ.get("WEATHER_API_KEY")
+    BASE_URL = "https://api.weatherapi.com/v1/"
+    CURRENT = "current.json"
+    PARAMS = f"?key={KEY}&q={city}"
+    URL = f"{BASE_URL}{CURRENT}{PARAMS}"
 
-response = requests.get(URL)
-# print(URL)
-json_response = json.loads(response.text)
-temp = json_response["current"]["temp_c"]
-feels_like = json_response["current"]["feelslike_c"]
-condition = json_response["current"]["condition"]["text"]
+    response = requests.get(URL)
+    json_response = json.loads(response.text)
+    country = json_response['location']['country']
+    temp = json_response["current"]["temp_c"]
+    feels_like = json_response["current"]["feelslike_c"]
+    condition = json_response["current"]["condition"]["text"]
+
+    current_weather = (
+        f"Current conditions for {city}, {country}:\n{condition}\nTemp: {temp}C\nFeels like: {feels_like}C")
+
+    return current_weather
 
 
-print(f"Current conditions for {city}, {json_response['location']['country']}:")
-print(condition)
-print(f"Temp: {temp}C")
-print(f"Feels like: {feels_like}C")
-
-
-# print(json_response)
+print(get_current_weather("Ottawa"))
