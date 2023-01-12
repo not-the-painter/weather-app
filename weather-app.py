9c3cd28d2a81531d323284332c12c2032357f11c
not_the_painter <bobross44@protonmail.com>
2023-01-11 21:58:46 -0500

    bind Enter keypress to get_user_input()

--- weather-app.py
+++ weather-app.py
@@ -3,7 +3,7 @@ from tkinter import *
 import weather
 
 
-def get_user_input():
+def get_user_input(event=None):
     show_weather = weather.get_current_weather(city_input.get())
     city_input.delete("0", END)
     show_lbl = Label(root, text=show_weather, justify='left')
@@ -20,6 +20,8 @@ city_lbl.grid(row=0, column=0)
 city_input = Entry(root)
 city_input.grid(row=0, column=1, columnspan=4)
 city_input.focus()
+
+city_input.bind('<Return>', get_user_input)
 
 get_city_btn = Button(root, text="Submit", command=get_user_input)
 get_city_btn.grid(row=1, column=1)
