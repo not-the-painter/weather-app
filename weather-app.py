ecb1d728276bf346ab60e524ae7f39c2ec415781
not_the_painter <bobross44@protonmail.com>
2023-01-23 20:17:07 -0500

    Fixed the overlapping label problem

--- weather-app.py
+++ weather-app.py
@@ -2,23 +2,31 @@ from tkinter import *
 
 import weather
 
+root = Tk()
+root.geometry("400x250")
+root.title('Simple Weather App')
+
+global show_lbl
+show_lbl = Label(root, text="", justify='left')
+
 
 def get_user_input(event=None):
     global show_lbl
     show_weather = weather.get_current_weather(city_input.get())
     city_input.delete("0", END)
-    show_lbl = Label(root, text=show_weather, justify='left')
-    show_lbl.grid(row=2, column=0, columnspan=4)
+    show_lbl.config(text=show_weather)
+    # show_lbl = Label(root, text=show_weather, justify='left')
+    show_lbl.grid(row=2, column=0, columnspan=5)
 
 
 def reset():
     global show_lbl
-    show_lbl.config(text="")
+    show_lbl.destroy()
 
 
-root = Tk()
-root.geometry("500x250")
-root.title('Simple Weather App')
+# root = Tk()
+# root.geometry("400x250")
+# root.title('Simple Weather App')
 
 city_lbl = Label(root, text='Enter the city: ')
 city_lbl.grid(row=0, column=0)
