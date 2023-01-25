96c44c3aecd30844f27e75126c198d404253d9fe
not_the_painter <bobross44@protonmail.com>
2023-01-25 18:06:31 -0500

    Wrapped everything into a function

--- weatrher-app.py
+++ weatrher-app.py
@@ -3,31 +3,36 @@ import PySimpleGUI as sg
 
 import weather
 
-layout = [
-    [sg.Text("Enter a city and :")],
-    [sg.Input(key="-IN-")],
-    [sg.Text(size=(30, 5), key="-OUT-")],
-    [sg.Button("Submit", bind_return_key=True),
-     sg.Button("Reset"), sg.Button("Exit")],
-]
 
-window = sg.Window("Weather App", layout)
+def weatherapp():
+    layout = [
+        [sg.Text("Enter a city or region:")],
+        [sg.Input(key="-IN-")],
+        [sg.Text(size=(40, 5), key="-OUT-")],
+        [sg.Button("Submit", bind_return_key=True),
+         sg.Button("Reset"), sg.Button("Exit")],
+    ]
 
-while True:
-    event, values = window.read()
-    print(event, values)
-    if event == sg.WIN_CLOSED or event == "Exit":
-        break
+    window = sg.Window("Weather App", layout)
 
-    window["-IN-"].update("")
+    while True:
+        event, values = window.read()
+        print(event, values)
+        if event == sg.WIN_CLOSED or event == "Exit":
+            break
 
-    if event == "Submit":
-        try:
-            window["-OUT-"].update(weather.get_current_weather(values['-IN-']))
-        except KeyError:
-            window["-OUT-"].update("Please enter a valid city.")
-    else:
-        window["-OUT-"].update("")
+        window["-IN-"].update("")
+
+        if event == "Submit":
+            try:
+                window["-OUT-"].update(weather.get_current_weather(values['-IN-']))
+            except KeyError:
+                window["-OUT-"].update("Please enter a valid city.")
+        else:
+            window["-OUT-"].update("")
+
+    window.close()
 
 
-window.close()
+if __name__ == "__main__":
+    weatherapp()
